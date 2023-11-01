09e36bfda4cbc7762ee3cb5ce9c31f5c89b8d62b
Cloynn_ <notcloynn@gmail.com>
2023-11-02 06:34:10 +0700

    using PlayCDN TailwindCSS
    Changed the use of TailwindCSS (previously using packages, now using PlayCDN) to make the development process easier

--- resources/views/Sekolah/index.blade.php
+++ resources/views/Sekolah/index.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('head')
-    @vite('resources/css/app.css') {{-- Tailwind CSS --}}
+    {{-- @vite('resources/css/app.css')  --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- Tailwind CSS --}}
     @yield('css') {{-- Custom CSS --}}
     <title>@yield('title', 'Izyankareem - Sch')</title>
 </head>
